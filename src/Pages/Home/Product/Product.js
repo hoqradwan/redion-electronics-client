72d3a34ac07bc81a90da75d8412a7f150dc7fa99
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-05-08 20:32:32 +0600

    code cleant

--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./product.css";
 
 const Product = ({ product }) => {
   const { _id, name, image, description, price, quantity, supplier } = product;
-  const { productId } = useParams();
   const navigate = useNavigate();
 
   const navigateToProductDetail = (id) => {
@@ -14,29 +13,30 @@ const Product = ({ product }) => {
   return (
     <div className="container product p-3 mt-3">
       <div className="row">
-      <div className="col-lg-5 mb-2 text-center">
-        <img
-       width={200}
-       height={200}
-          src={image}
-          className="img-fluid"
-          alt="..."
-        />
-      </div>
-      <div className="col-lg-7">
-        <h5 className="lh-sm">{name}</h5>
-        <p className="lh-sm">{description}</p>
-        <p className="fw-bold fs-4 mb-4 price">${price}</p>
-        <p className="lh-1">Quantity: {quantity}</p>
-        <p className="lh-1 text-muted fs-6">{supplier}</p>
+        <div className="col-lg-5 mb-2 text-center">
+          <img
+            width={200}
+            height={200}
+            src={image}
+            className="img-fluid"
+            alt="..."
+          />
+        </div>
+        <div className="col-lg-7">
+          <h5 className="lh-sm">{name}</h5>
+          <p className="lh-sm">{description}</p>
+          <p className="fw-bold fs-4 mb-4 price">${price}</p>
+          <p className="lh-1">Quantity: {quantity}</p>
+          <p className="lh-1 text-muted fs-6">{supplier}</p>
 
-        <button onClick={() => navigateToProductDetail(_id)} className="update">
-          Manage
-        </button>
+          <button
+            onClick={() => navigateToProductDetail(_id)}
+            className="update"
+          >
+            Manage
+          </button>
+        </div>
       </div>
-      </div>
-     
-     
     </div>
   );
 };
